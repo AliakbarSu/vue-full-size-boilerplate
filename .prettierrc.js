ba64f0ab5fb448fbbc556601a170f36fdfcc0a53
Ali Sultani <ali.sultani@abley.com>
2022-07-27 15:35:51 +1200

    Add prettier and eslint

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 160,
+  arrowParens: 'always',
+  useTabs: true,
+  semi: true,
+  singleQuote: false,
+  bracketSpacing: true,
+  bracketSameLine: true,
+  endOfLine: 'auto',
+  tabWidth: 2
+}
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from 'src/store/index'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "@/store";
 
-import './assets/main.css'
+import "./assets/main.css";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(store)
-app.use(router)
+app.use(store);
+app.use(router);
 
-app.mount('#app')
+app.mount("#app");
